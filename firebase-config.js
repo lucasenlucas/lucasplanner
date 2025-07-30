32576d0ffa5122859ce76c162d4a3bc7b01933e1
Lucas <96977171+lucasenlucas@users.noreply.github.com>
2025-07-30 22:27:13 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyBes5kIKqyJtxjN-n8iRia011tCeibykRg",
+  authDomain: "lucas-planner.firebaseapp.com",
+  projectId: "lucas-planner",
+  storageBucket: "lucas-planner.appspot.com",
+  messagingSenderId: "39602633137",
+  appId: "1:39602633137:web:2a729e3b293ca162e7e590"
+};
