2e4408379894602cdfcc548f3600c7737947fa77
Lucas <96977171+lucasenlucas@users.noreply.github.com>
2025-07-30 22:26:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { firebaseConfig } from "./firebase-config.js";
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Login formulier
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => window.location.href = "index.html")
+      .catch(err => alert("Login mislukt: " + err.message));
+  });
+}
